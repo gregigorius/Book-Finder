10b06861e08fcf110a248a044f8bdb5f61dc0c7e
Ichsan <ichsan.junaid@inmotion.co.id>
2021-11-11 12:06:38 +0700

    update env.production

--- src/views/API.js
+++ src/views/API.js
@@ -1,62 +1,3 @@
-// import axios from "axios";
-
-
-// const apiEndpoint = `${process.env.REACT_APP_REACT_APP_BASE_URL}/dolphin/apiv1/graph/workflow`;
-// const channelApi = `${process.env.REACT_APP_WB_GET_CHANNEL_NAME}`;
-// const groupApi = `${process.env.REACT_APP_WB_GET_GROUP}`;
-// const agentNameApi = `${process.env.REACT_APP_WB_GET_AGENT}`;
-// const ratingApi = `${process.env.REACT_APP_WB_GET_RATING}`;
-// const userApi = `${process.env.REACT_APP_WB_GET_USER}`;
-
-
-// const headers = {
-//   "Content-Type": "application/json",
-// };
-
-// const getData = async (url, body, callback, errorCallback) => {
-//   try {
-//     const response = await axios.post(url, body, headers);
-//     callback(response.data);
-//   } catch (error) {
-//     errorCallback(error);
-//   }
-  
-// };
-
-
-// export const getChannelNameAPI = (payload, callback, errorCallback) => {
-//   const url = `${apiEndpoint}${channelApi}`;
-//   const body = JSON.stringify(payload);
-//   getData(url, body, callback, errorCallback);
-//   console.log("testttttt"+ ` ${channelApi}`)
-// }
-
-// export const getGroupAPI = (payload, callback, errorCallback) => {
-//   const url = `${apiEndpoint}${groupApi}`;
-//   const body = JSON.stringify(payload);
-//   getData(url, body, callback, errorCallback);
-// }
-
-// export const getAgentNameAPI = (payload, callback, errorCallback) => {
-//   const url = `${apiEndpoint}${agentNameApi}`;
-//   const body = JSON.stringify(payload);
-//   getData(url, body, callback, errorCallback);
-// }
-
-// export const getRatingAPI = (payload, callback, errorCallback) => {
-//   const url = `${apiEndpoint}${ratingApi}`;
-//   const body = JSON.stringify(payload);
-//   getData(url, body, callback, errorCallback);
-// }
-
-// export const getUserAPI = (payload, callback, errorCallback) => {
-//   const url = `${apiEndpoint}`+`${userApi}`; 
-  
-//   const body = JSON.stringify(payload);
-//   getData(url, body, callback, errorCallback);
-//   console.log(body);
-// }
-
 import axios from "axios";
 
 
@@ -78,31 +19,31 @@ const getData = async (url, body, callback, errorCallback) => {
 };
 
 
-export const getChannelNameAPI = (payload, callback, errorCallback) => {
+export const getListChannelNameAPI = (payload, callback, errorCallback) => {
   const url = `${apiEndpoint}`+process.env.REACT_APP_WB_GET_CHANNEL_NAME;
   const body = JSON.stringify(payload);
   getData(url, body, callback, errorCallback);
 }
 
-export const getGroupAPI = (payload, callback, errorCallback) => {
+export const getListGroupNameAPI = (payload, callback, errorCallback) => {
   const url = `${apiEndpoint}`+process.env.REACT_APP_WB_GET_GROUP;
   const body = JSON.stringify(payload);
   getData(url, body, callback, errorCallback);
 }
 
-export const getAgentNameAPI = (payload, callback, errorCallback) => {
+export const getListAgentNameAPI = (payload, callback, errorCallback) => {
   const url = `${apiEndpoint}`+process.env.REACT_APP_WB_GET_AGENT_NAME;
   const body = JSON.stringify(payload);
   getData(url, body, callback, errorCallback);
 }
 
-export const getRatingAPI = (payload, callback, errorCallback) => {
+export const getListRatingAPI = (payload, callback, errorCallback) => {
   const url = `${apiEndpoint}`+process.env.REACT_APP_WB_GET_AGENT_NAME;
   const body = JSON.stringify(payload);
   getData(url, body, callback, errorCallback);
 }
 
-export const getUserAPI = (payload, callback, errorCallback) => {
+export const getListUserAPI = (payload, callback, errorCallback) => {
   const url = `${apiEndpoint}`+process.env.REACT_APP_WB_GET_USER;
   const body = JSON.stringify(payload);
   getData(url, body, callback, errorCallback);
