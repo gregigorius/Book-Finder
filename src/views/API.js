0475536c9900e258cda8541b704f757fc8c55899
Mj <ichsan.junaid@inmotion.co.id>
2022-03-30 14:45:06 +0700

    Dashboard V2 (comment popup, field & coloumn)

--- src/views/API.js
+++ src/views/API.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-
 const apiEndpoint = `${process.env.REACT_APP_BASE_URL}/dolphin/apiv1/graph/workflow`;
-
-
 
 const headers = {
   "Content-Type": "application/json",
@@ -18,34 +15,32 @@ const getData = async (url, body, callback, errorCallback) => {
   }
 };
 
-
 export const getListChannelNameAPI = (payload, callback, errorCallback) => {
-  const url = `${apiEndpoint}`+process.env.REACT_APP_WB_GET_CHANNEL_NAME;
+  const url = `${apiEndpoint}` + process.env.REACT_APP_WB_GET_CHANNEL_NAME;
   const body = JSON.stringify(payload);
   getData(url, body, callback, errorCallback);
-}
+};
 
 export const getListGroupNameAPI = (payload, callback, errorCallback) => {
-  const url = `${apiEndpoint}`+process.env.REACT_APP_WB_GET_GROUP;
+  const url = `${apiEndpoint}` + process.env.REACT_APP_WB_GET_GROUP;
   const body = JSON.stringify(payload);
   getData(url, body, callback, errorCallback);
-}
+};
 
 export const getListAgentNameAPI = (payload, callback, errorCallback) => {
-  const url = `${apiEndpoint}`+process.env.REACT_APP_WB_GET_AGENT_NAME;
+  const url = `${apiEndpoint}` + process.env.REACT_APP_WB_GET_AGENT_NAME;
   const body = JSON.stringify(payload);
   getData(url, body, callback, errorCallback);
-}
+};
 
 export const getListRatingAPI = (payload, callback, errorCallback) => {
-  const url = `${apiEndpoint}`+process.env.REACT_APP_WB_GET_AGENT_NAME;
+  const url = `${apiEndpoint}` + process.env.REACT_APP_WB_GET_RATING;
   const body = JSON.stringify(payload);
   getData(url, body, callback, errorCallback);
-}
+};
 
 export const getListUserAPI = (payload, callback, errorCallback) => {
-  const url = `${apiEndpoint}`+process.env.REACT_APP_WB_GET_USER;
+  const url = `${apiEndpoint}` + process.env.REACT_APP_WB_GET_USER;
   const body = JSON.stringify(payload);
   getData(url, body, callback, errorCallback);
-}
-
+};
